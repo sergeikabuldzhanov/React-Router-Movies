bba488f3ba3979df95c3683f5feae3d61a8e39e1
sergeikabuldzhanov <thesiegebot@gmail.com>
2019-11-11 17:35:19 +0500

    Did second MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
+import {Route, Link} from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import MovieCard from './Movies/MovieCard';
+import Movie from './Movies/Movie';
+
 
 import SavedList from './Movies/SavedList';
 
@@ -12,7 +17,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path = "/" component = {MovieList}/>
+      <Route exact path = "/movies/:id" component = {Movie}/>
     </div>
   );
 };
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
- 
+  const [movie, setMovie] = useState();
+  console.log(movie);
+  
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
+    console.log(id);
+    
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
